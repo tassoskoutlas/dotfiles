8f6f7a03f7a12d5f37edeb51063d866ab0a5c9ce
Tassos Koutlas <tassos.koutlas@gmail.com>
2016-08-31 08:01:51 +0100

    Updates the disbale script with information on tis usage.

--- disable_trackpad.sh
+++ disable_trackpad.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
+# Disable trackpad on a X230 ThinkPad.
+#
+# On KDE use system settings to place to autostart this script. Alternatively
+# it can be linked in ~/.config/autostart-scripts/.
+
 xinput set-prop 11 'Device Enabled' 0
 exit 0
